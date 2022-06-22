567ace975c2c0e4f80e69da6b0d2ea507baf557a
Travis <tzito93@gmail.com>
2022-06-22 14:56:59 -0500

    refactored form from new and edit views into form partial

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
-  validates :username, :email, :password, presence: true, length: { minimum: 3, maximum: 20 }
+  validates :username, presence: true, length: { minimum: 6, maximum: 20 }
+  validates :email, presence: true, length: { minimum: 5, maximum: 40 }
+  validates :password, presence: true, length: { minimum: 8, maximum: 20 }
 end
