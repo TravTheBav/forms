3c7236c59ff66749d04759504522024f54cfa13c
Travis <tzito93@gmail.com>
2022-06-22 14:34:12 -0500

    errors messages for failed user edits

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,3 @@
 class User < ApplicationRecord
-  validates :username, :email, :password, presence: true
+  validates :username, :email, :password, presence: true, length: { minimum: 3, maximum: 20 }
 end
